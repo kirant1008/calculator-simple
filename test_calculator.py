1a40a6e29fb9b3762e748798998b87032256c2f4
Kiran Tulsulkar <kirantulsulkar@gmail.com>
2025-04-26 12:55:14 +0530

    add deployment

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,13 @@
 from calculator import add, subtract
 
 def test_add():
+    """Test the add function."""
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
 
 def test_subtract():
-
+    """Test the subtract function."""
     assert subtract(5, 3) == 2
     assert subtract(0, 0) == 0
     assert subtract(-1, -1) == 0
